e61b0cd70cb5114e65b3c0d605b8d69ccbe1fdda
popostrus <alfredofloresn@hotmail.com>
2017-06-04 02:36:08 -0500

    0.2

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ console.log("test1");
 
 
     function myFunction(position){
+
        console.log("test2");
       $("#content").remove();
       $("body").append("<div class='' id='content'></div>");
@@ -39,41 +40,49 @@ console.log("test1");
         types: [typeSelected]
       };
       var service=new google.maps.places.PlacesService(document.createElement('div'));
-      service.nearbySearch(request,callback);
+
+        service.nearbySearch(request,callback);
+
+
 
       function callback(results,status){
-        for (var i = 0; i < results.length; i++) {
-          var colorClass;
-          if (i%2==0){
-            colorClass="content-div1";
-          }
-          else{
-            colorClass="content-div2";
-          }
 
-          if (results[i].rating>=istars){
-            var rq={
-            placeId:results[i].place_id
-            };
+                GetData(results);
 
-            service.getDetails(rq, callback1);
+      }
 
-            function callback1(place, status) {
-              console.log(place);
+      function GetData(data){
+        var aData=data;
+        var i;
+        var j=0;
 
 
 
+        var tm=250;
+        for (i = 0; i < aData.length; i++) {
+          setTimeout(function () {
 
-
-              $('#content').append(
-                "<div class='col-xs-12 "+ colorClass+"'>"+
-                "<a href='"+place.website+"'><img class='content-image' src="+"'"+place.photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200})+"'"+"height='100' width='100'> </a>"+
-                "<h2 class='content-text'>"+place.name+"</h2>"+
-                "<br>"+
-                "</div>"
-              );
-            }
-          }
+            FindLocation(aData[j]);
+            j++
+          },tm);
+          tm+=250;
         }
       }
+
+      function FindLocation(place){
+          var rq={
+            placeId:place.place_id
+          };
+          service.getDetails(rq, function(details, status){
+            console.log(details.name);
+            $('#content').append(
+              "<div class='col-xs-12 "+ "'>"+
+              "<a href='"+details.website+"'><img class='content-image' src="+"'"+details.photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200})+"'"+"height='100' width='100'> </a>"+
+              "<h2 class='content-text'>"+details.name+"</h2>"+
+              "<br>"+
+              "</div>"
+            );
+          });
+      }
+
     }
